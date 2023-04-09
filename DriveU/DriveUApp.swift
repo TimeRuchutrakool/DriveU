b7084d0ffa67a2a27e9d7601babc8c7175e11f19
tharmJaa <timeruchu0808@gmail.com>
2023-04-09 15:13:18 +0700

    Initial Commit

---
+++ DriveU/DriveUApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveUApp.swift
+//  DriveU
+//
+//  Created by Time Ruchutrakool on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DriveUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
